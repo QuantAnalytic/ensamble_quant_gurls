0053f518462762bce6e22fd4e4ffc27b34fbe034
Antoni Kowalczuk <antonikowalczuk@gmail.com>
2024-03-10 11:14:09 +0000

    add defensetransform data load

---
+++ defensetransformation/load_data.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+if __name__ == "__main__":
+    data = np.load("defensetransformation/data/ExampleDefenseTransformationEvaluate.npz")
+    print(data["labels"], data["representations"].shape)
+    
+    data = np.load("defensetransformation/data/ExampleDefenseTransformationSubmit.npz")
+    print(data["representations"].shape)
