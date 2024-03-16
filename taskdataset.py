c3265aef31d176fb5bbed9a7c13ff6ad59e4afd4
JakubFgit <jakub.frydrych@op.pl>
2024-03-16 15:36:30 +0000

    rozszerzenia datasetu

--- taskdataset.py
+++ taskdataset.py
@@ -5,17 +5,20 @@ import torch
 
 class TaskDataset(Dataset):
     def __init__(self, transform=None):
-
         self.ids = []
         self.imgs = []
         self.labels = []
-
         self.transform = transform
+
+    def add_image(self, image, label):
+        self.ids.append(len(self.ids))  # Dodaj unikalny ID oparty na aktualnej długości listy
+        self.imgs.append(image)  # Dodaj obraz
+        self.labels.append(label)  # Dodaj etykietę
 
     def __getitem__(self, index) -> Tuple[int, torch.Tensor, int]:
         id_ = self.ids[index]
         img = self.imgs[index]
-        if not self.transform is None:
+        if self.transform is not None:
             img = self.transform(img)
         label = self.labels[index]
         return id_, img, label
